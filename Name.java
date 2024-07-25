3e3eae4f53998053bd00c01d153d73165cf14509
Siddhesh Bhalerao <156121006+SIDDHESH9665@users.noreply.github.com>
2024-07-25 15:14:54 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Name {
+    public static void main(String[] args){
+        // String name = "Hello";
+        // System.out.println(name);
+
+        Scanner sc = new Scanner(System.in);
+        String  name = sc.nextLine();
+        System.out.println(name);
+    }
+}
+
+
+Scanner sc = new Scanner(System.in);
