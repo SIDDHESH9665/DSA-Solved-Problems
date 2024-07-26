c9868bbeb71da89c9faee2bc26f2d1202d139b80
Siddhesh Bhalerao <156121006+SIDDHESH9665@users.noreply.github.com>
2024-07-26 15:18:06 +0530

    Add files via upload

---
+++ PreFixSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class PreFixSum {
+
+    public static int preFixSum(int arr[])
+    {
+        int maxSum = Integer.MIN_VALUE;
+         int currSum =0;
+         int preFixarr []= new int[arr.length];
+
+         for(int i=1;i<arr.length;i++)
+         {
+            
+         }
+    }
+
+    public static void main(String args[])
+    {
+        int arr[]={1,-2,6,-1,3};
+        preFixSum(arr);
+    }
+}
