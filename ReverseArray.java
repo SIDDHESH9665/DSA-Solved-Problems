396572a08646ca05526b511337178e65e66b7d0a
Siddhesh Bhalerao <156121006+SIDDHESH9665@users.noreply.github.com>
2024-07-27 16:23:58 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ReverseArray {
+
+    public static void reverseArray(int arr[])
+    {
+        int start =0,end=arr.length-1;
+        while (start<end) {
+             arr[start] = arr[start] + arr[end];
+             arr[end] = arr[start] -arr[end];
+             arr[start] = arr[start] -arr[end];
+             start++;
+             end--;
+
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int arr[] = {2,4,6,8,10};
+
+        reverseArray(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
