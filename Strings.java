881d75a9129a58928ff0b37aab4b884859327e78
Siddhesh Bhalerao <156121006+SIDDHESH9665@users.noreply.github.com>
2024-08-01 18:39:34 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = new String();
+        String name;
+        name = sc.next();
+        System.out.println(name);
+
+        String name1 = "Siddhesh";
+        System.out.println(name1);
+    }
+}
